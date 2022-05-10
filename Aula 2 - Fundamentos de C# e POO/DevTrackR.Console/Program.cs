3afef7bcec407d09bcfa5442c094e3e7c7c65df8
Francisco Rafael <francisco_rafael@hotmail.com.br>
2022-05-10 10:31:56 -0300

    tipos de dados

--- Aula 2 - Fundamentos de C# e POO/DevTrackR.Console/Program.cs
+++ Aula 2 - Fundamentos de C# e POO/DevTrackR.Console/Program.cs
@@ -3,3 +3,15 @@
 var nome = Console.ReadLine();
 
 Console.WriteLine($"Olá, {nome}");
+
+var umaString = "Essa é uma String";
+var numeroInt = 5;
+var numeroFloar = 5.4f;
+var numeroDouble = 5.4;
+var numeroDecimal = 5.3m;
+var array = new int[3] { 1, 2, 3 };
+var caractere = 'a'; //caractere com as aspas simples
+var date = new DateTime(1992, 12, 1);
+
+
+
